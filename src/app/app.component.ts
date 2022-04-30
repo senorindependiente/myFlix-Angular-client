f393273cc82358d8d6faaa0fedfb4be3aafb96d8
senorindependiente <pingelchristian@hotmail.com>
2022-04-30 14:45:08 +0200

    Add movie-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -19,4 +20,11 @@ openUserRegistrationDialog(): void {
     width: '280px'
     });
   }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
+
 }
